039a7f0b7454478c29752fd0b7962ee271f2091d
Sophie Orta <sophie@travis-ci.org>
2020-09-15 10:51:41 -0400

    scope to rest

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :members
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope :rest do
+    resources :members
+  end
 end
