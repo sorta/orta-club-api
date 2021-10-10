c8c496be8753b9aa68203a77602ed9de8bfb3a82
sorta <orta.sophie@gmail.com>
2021-10-10 00:58:34 -0400

    Add seeding from CSVs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+SEEDED_MODELS = [
+  :members
+].freeze
+
+SEEDED_MODELS.each do |model_name|
+  csv_text = File.read(Rails.root.join('db', 'seeds', "#{model_name}.csv"))
+  csv_data = CSV.parse(csv_text, :headers => true)
+
+  csv_data.each do |row|
+    case model_name
+    when :members
+      member = Member.new
+      member.name_first = row['name_first']
+      member.name_middle = row['name_middle']
+      member.name_last = row['name_last']
+      member.birthdate = row['birthdate']
+      member.is_approved = true
+      member.save
+
+      if row['user_email'].present? && row['user_password'].present?
+        user = User.new
+        user.member = member
+        user.email = row['user_email']
+        user.password = row['user_password']
+        user.is_admin = row['user_is_admin'] == 'TRUE'
+        user.save
+      end
+    else
+      raise 'Unknown Model!'
+    end
+  end
+end
