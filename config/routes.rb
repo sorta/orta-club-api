71ed696ee09e639f07c65fd68a3e18f91b4117ec
sorta <orta.sophie@gmail.com>
2021-11-07 13:31:05 -0500

    Add years to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :donnings
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :years
   resources :members
   resources :users do
     collection do
